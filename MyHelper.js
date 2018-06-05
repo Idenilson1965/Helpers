83e8595fe9b0518e8cb19db39307cf8993f247ad
Peterson Passos <peterson.jfp@gmail.com>
2018-06-05 14:25:16 -0300

    adicionadas funções js no helper

--- MyHelper.js
+++ MyHelper.js
@@ -6,3 +6,29 @@ function numberToReal(numero) {
     numero[0] = "R$ " + numero[0].split(/(?=(?:...)*$)/).join('.');
     return numero.join(',');
 }
+
+function isFloat(n){
+	return n === +n && n !== (n|0);
+}
+
+function isInteger(n){
+	return n === +n && n === (n|0);
+}
+
+//mascara e desmascara um float como real brasileiro
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-maskmoney/3.0.2/jquery.maskMoney.min.js" type="text/javascript"></script>
+
+function mascarar(id){
+	$(".real").maskMoney({
+		prefix: "R$ ",
+		decimal: ",",
+		thousands: "."
+	});
+	$(id).maskMoney('mask');
+}
+
+function desmascarar(string){
+	var num = parseFloat(string.replace('R$ ', '').replace('.', '').replace(',', '.'));
+	return num;
+}
+
